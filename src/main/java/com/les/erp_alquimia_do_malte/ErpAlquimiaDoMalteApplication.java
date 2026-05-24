9b54993953b865de949904771e1ad985a050e4c2
Bruna Midori <brunamidoriyy@yahoo.com.br>
2026-05-23 22:42:22 -0300

    init projeto

---
+++ src/main/java/com/les/erp_alquimia_do_malte/ErpAlquimiaDoMalteApplication.java
@@ -0,0 +1,13 @@
+package com.les.erp_alquimia_do_malte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpAlquimiaDoMalteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpAlquimiaDoMalteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/les/erp_alquimia_do_malte/ErpAlquimiaDoMalteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.les.erp_alquimia_do_malte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpAlquimiaDoMalteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
